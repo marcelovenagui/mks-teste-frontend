0854989d9e03e037ba8ad3df2f7d59f7496c93d8
marcelovenagui <marcelovenancioaguiar@gmail.com>
2022-11-22 00:35:52 -0300

    Navbar concluída!

---
+++ mks-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Navbar.css"
+
+export default function index() {
+  return (
+    <div className='header'>
+      
+          <h1>MKS</h1>
+          <h3>Sistemas</h3>
+          <button className='button'>
+              🛒  0
+          </button>
+          
+    </div>
+  )
+}
